70f82df5a001006681d354cfd31453e49955b575
Thiago Diniz <thiagodiniz@Thiagos-MacBook-Pro.local>
2018-08-16 16:48:12 -0300

    initial commit

--- LottieTest/ViewController.swift
+++ LottieTest/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    let animationView = LOTAnimationView(name: "tutorial")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        pageControl.numberOfPages = 6
+        setupAnimation()
+        setupScroll()
     }
+    
+    func setupScroll() {
+        scrollView.delegate = self
+        scrollView.contentSize = CGSize(width: self.view.frame.size.width * 6, height: scrollView.frame.size.height)
+    }
+    
+    func setupAnimation() {
+        animationView.frame = CGRect(x: 0, y: 50, width: self.view.frame.size.width, height: 300)
+        animationView.contentMode = .scaleAspectFill
+        animationView.loopAnimation = true
+        view.addSubview(animationView)
+    }
+}
 
-
+extension ViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let progress = scrollView.contentOffset.x / scrollView.contentSize.width
+        animationView.animationProgress = progress
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let pageIndex = round(scrollView.contentOffset.x/view.frame.width)
+        pageControl.currentPage = Int(pageIndex)
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '11.0'
+
+target 'LottieTest' do
+  use_frameworks!
+  pod 'lottie-ios'
+end
